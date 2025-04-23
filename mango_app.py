a70fec0779c80a0fdfa30ae52ceb789157a32853
rpna24 <rolce.an11@gmail.com>
2025-04-23 16:43:22 +1000

    Update mango_app.py

--- mango_app.py
+++ mango_app.py
@@ -182,7 +182,7 @@ import os
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
     
 
 
